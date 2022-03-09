3d8a5b16a5e6d43954c8fb700231219deb97256a
imjord <dean.jay.goat@gmail.com>
2022-03-08 18:08:42 -0700

    complete funcunality

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // dom elements 
-
 const imageContainerEl = document.getElementById('image-container')
 const loader = document.getElementById('loader')
-
-
-
-
-
-
-
-
-
 // unsplasb image api 
-const count = 1;
+const count = 10;
 const apikey = 'oEqFxDg3cc7z5ywAbFjc4lw4G-C5dE8usaQ5l0Too3w' // dont steal pl0x
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${count}`;
-
 let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
+function dataLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
 
+        ready = true;
+        loader.hidden = true;
+      
+    }
+}
 // helper function to set attribbutes on dom elemts
 function setAttributes(element, attributes){
     for (const key in attributes){
         element.setAttribute(key, attributes[key]);
     }
 }
-
 function displayPhotos(){
-
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         //create <a> link to unsplash
         const item = document.createElement('a');
@@ -47,44 +47,30 @@ function displayPhotos(){
             alt: photo.alt_description,
             title: photo.alt_description,
         })
+        imag.addEventListener('load', dataLoaded());
         // apend our img to the a link and then put them in the container
         item.appendChild(imag);
         imageContainerEl.appendChild(item);
-
-
     });
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
 async function getPhotos(){
-
-
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        // console.log(photosArray);
+ 
     displayPhotos();        
     } catch(error){
         console.log(error);
 
     }
 }
+// check to see if scrolling near bottom of page load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos();
 
-
-
-
-
+    }
+})
 
 getPhotos();
